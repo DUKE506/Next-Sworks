cbe155f84331c9ed6415c0046c64f39db591e1f7
DUKE506 <dukebs1128@gmail.com>
2025-04-03 23:57:23 +0900

    feat : setting

---
+++ src/app/(admin)/manage/_components/SideBar/side-bar.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+export const SideBar = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>프로필 영역</CardTitle>
+      </CardHeader>
+      <CardContent>메뉴 영역</CardContent>
+    </Card>
+  );
+};
---
+++ src/app/(admin)/manage/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SideBar } from "./_components/SideBar/side-bar";
+
+const layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex h-full p-4 gap-4">
+      <div>
+        <SideBar />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default layout;
